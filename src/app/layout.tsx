aff019fc508c7281e5c87b0e33577c6648154055
Henco Fouche <hencofouche8@gmail.com>
2025-08-30 18:44:16 +0000

    Create pwa for the app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from '@/components/theme-provider';
 export const metadata: Metadata = {
   title: 'TPSA Calculator',
   description: 'The TPSA Calculator.',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -17,7 +18,6 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/icon.svg" type="image/svg+xml" />
-        <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#000000" />
         <link rel="apple-touch-icon" href="/icon.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
