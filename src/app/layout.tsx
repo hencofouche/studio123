266a27604802b8af01c40105045a60c03302f8fc
Henco Fouche <hencofouche8@gmail.com>
2025-08-31 09:16:43 +0000

    i added icons so need for you svg versions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,9 +24,9 @@ export default function RootLayout({
         <title>TPSA Calculator</title>
         <meta name="description" content="The TPSA Calculator." />
         <link rel="manifest" href="/manifest.json" />
-        <link rel="icon" href="/icon.svg" type="image/svg+xml" />
-        <link rel="apple-touch-icon" href="/icon.svg" />
-        <meta name="theme-color" content="#4285F4" />
+        <link rel="icon" href="/icons/icon.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <meta name="theme-color" content="#FDB813" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
