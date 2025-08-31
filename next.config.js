f2221ba79c06eca7f1ed7658cb3afda086e3beaa
Henco Fouche <hencofouche8@gmail.com>
2025-08-31 09:37:41 +0000

    The site I was testing is: https://studio123-ten.vercel.app/ at Microsof

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const withPWA = require("next-pwa")({
   disable: process.env.NODE_ENV === "development",
   runtimeCaching: [
     {
-      urlPattern: /^https?:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
+      urlPattern: /^https:\/\/fonts\.(?:googleapis|gstatic)\.com\/.*/i,
       handler: 'CacheFirst',
       options: {
         cacheName: 'google-fonts',
