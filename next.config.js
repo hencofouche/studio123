66db629996df955206733c4ef19f5a120fb4d00c
Henco Fouche <hencofouche8@gmail.com>
2025-08-31 14:38:02 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.js
+++ next.config.js
@@ -23,7 +23,7 @@ const nextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https'
+        protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
