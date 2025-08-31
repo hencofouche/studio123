1d01ae7ef33d25bfdac474869b1c64be45c48b5e
Henco Fouche <hencofouche8@gmail.com>
2025-08-31 14:34:59 +0000

    there is no pwa createwd lol

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const withPWA = require("next-pwa")({
   dest: "public",
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
 });
 
 const nextConfig = {
@@ -22,7 +23,7 @@ const nextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https',
+        protocol: 'https'
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,8 @@ export default function RootLayout({
         <title>CalcForge</title>
         <meta name="description" content="A calculator for all your forging needs." />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#FDB813" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png"></link>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
