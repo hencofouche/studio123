eea2e9d0b4fbe59cf556b3e0bee05ede7044407e
Henco Fouche <hencofouche8@gmail.com>
2025-09-08 12:37:37 +0000

    The service worker has nothing to do with this I uninstalled the app the

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -98,17 +98,6 @@ export default function Home() {
 
     window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
 
-    // Register service worker
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/sw.js').then(function(registration) {
-          console.log('Service Worker registration successful with scope: ', registration.scope);
-        }, function(err) {
-          console.log('Service Worker registration failed: ', err);
-        });
-      });
-    }
-
     return () => {
       window.removeEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
     };
