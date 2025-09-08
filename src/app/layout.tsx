72da047979fa5bd03a147033b990470b87d3a06c
Ideality Creations <clashfouche@gmail.com>
2025-09-08 09:18:16 +0000

    I added an icon image for the pwa in the public folder create an AWP wit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,8 +24,8 @@ export default function RootLayout({
         <title>CalcForge</title>
         <meta name="description" content="A calculator for all your forging needs." />
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#000000" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png"></link>
+        <meta name="theme-color" content="#F0F4FF" />
+        <link rel="apple-touch-icon" href="/icon.jpg"></link>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
